1a983dea06637d6d78db3d0ef9639c0b3269b77f
rafaelsouzars <rafael21.santos@hotmail.com>
2026-08-27 16:58:18 -0300

    :recycle: refactor(editor): Window Dialogs Description: Replace show methods windows dialogs. Author: @rafaelsouzars

--- src/mainview/utils/index.tsx
+++ src/mainview/utils/index.tsx
@@ -1,6 +1,7 @@
 export { WindowAbout } from './WindowAbout';
 export { WindowAlert } from './WindowAlert';
 export { WindowDialog } from './WindowDialog';
+export { WindowConfirm } from './WindowConfirm';
 export { WindowSelectList } from './WindowSelectList';
 export { 
     fileNamePathToExtension
